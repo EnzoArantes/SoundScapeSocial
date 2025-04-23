5078f08cf0b3a4aebbc475dbe743c95daad1594d
Zo-hack <enzoarantes2000@gmail.com>
2025-04-23 12:08:47 -0400

    Initial Commit

---
+++ SoundScapeSocial/SoundScapeSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundScapeSocialApp.swift
+//  SoundScapeSocial
+//
+//  Created by Enzo Arantes on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SoundScapeSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
